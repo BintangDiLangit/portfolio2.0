46e87582abbd63a697a375bf3b83d954e6450088
mugiwara <mugiwara@localhost.localdomain>
2022-12-03 12:11:12 +0700

    sitemap

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +24,18 @@ Route::get('/', function () {
 Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
 Route::get('/detail-portfolio/{id}', [WelcomeController::class, 'detailPortfolio'])->name('detail');
 Route::post('/email', [WelcomeController::class, 'sendEmail'])->name('send.email');
+
+Route::get('sitemap', function () {
+
+    $site = App::make('sitemap');
+    $site->add(URL::to('/'), date("Y-m-d h:i:s"), 1, 'daily');
+
+    $responsePortfolio = Http::post(env('APP_URL_API') . '/api/v1/all-portfolios');
+    $portfolios = $responsePortfolio->json()["data"];
+
+    foreach ($portfolios as $key => $value) {
+        $site->add(URL::to('/detail-portfolio/' . $value['id']), $value['created_at'], 1, 'daily');
+    }
+
+    $site->store('xml', 'sitemap');
+});
